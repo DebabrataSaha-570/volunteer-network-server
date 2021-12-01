ad28e925cf4e6e17f8a9836ecee9db8192061714
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-02 00:48:21 +0600

    post api created

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imkxn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri)
+// console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -21,22 +21,21 @@ async function run() {
         const eventCollection = database.collection("Events");
         // create a document to insert
         app.post('/addEvent', async (req, res) => {
-
+            const event = req.body;
+            console.log(event)
+            const result = await eventCollection.insertOne(event)
+            res.json(result)
+            console.log(`A document was inserted with the _id: ${result.insertedId}`);
         })
 
-        // const doc = {
-        //     title: "Record of a Shriveled Datum",
-        //     content: "No bytes, no problem. Just insert a document, in MongoDB",
-        // }
-        // const result = await eventCollection.insertOne(doc);
-        // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Volunteers!!')
 })
 
 app.listen(port, () => {
